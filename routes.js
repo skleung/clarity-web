3f08f5b9e55723f966bf58d3e277aacb4cc2e14c
Sherman Leung <shermankleung@gmail.com>
2014-08-29 01:26:25 -0400

    added soundcloud

--- routes.js
+++ routes.js
@@ -2,4 +2,10 @@ module.exports = function(app, nconf) {
   app.get('/', function(req, res) {
     res.render('index');
   });
+
+  app.post('/search', function(req, res) {
+    debugger;
+    console.log(req);
+    
+  });
 };
