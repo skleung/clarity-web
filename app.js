388f37eb0214d49d031f0a706157dcad806d4f13
Sherman Leung <shermankleung@gmail.com>
2014-11-19 03:11:21 -0800

    first commit

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ require('./routes/index.js')(app);
 
 server.listen(process.env.PORT || 3000);
 console.log('Listening at 127.0.0.1:' + 3000);
+
+var passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy;
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
