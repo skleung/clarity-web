7e311f1b9ee8d2280983b5004676b62e8dd79649
gnedivad <gnedivad@gmail.com>
2014-11-20 00:41:49 -0800

    renamed post to question

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var Question = require('../models/question');
+
 module.exports = function(app) {
   /* Renders the index and landing page. */
   app.get('/', function(request, response) {
@@ -9,46 +11,55 @@ module.exports = function(app) {
     response.render('login.html');
   });
 
+  /*
+   * Teachers
+   */
+
   /* rendering dashboard page */
   app.get('/teacher/dashboard', function(request, response) {
     response.render('teacher/teacher-dashboard.html');
   });
+
+  /* rendering start page */
+  app.get('/start', function(request, response) {
+    response.render('teacher/start.html');
+  });
+
+
+  /*
+   * Students
+   */
 
   /* Renders faux-student view */
   app.get('/student/dashboard', function(request, response) {
     response.render('student/student-dashboard.html');
   });
 
-  /* Handles student post content */
-  app.post('/student/posts', function() {
+  /* Handles student question content */
+  app.post('/student/questions', function() {
     if (!request.body.content) {
       response.send(422, 'Must provide content.');
     }
 
-    var post = new Post({
+    var question = new Question({
       content: request.body.content,
       active: true,
       upvotes: 0
     });
 
-    post.save(function(error, post) {
+    question.save(function(error, question) {
       if (error) {
         throw error;
       } else {
-        response.json(200, post);
+        response.json(200, question);
       }
     });
 
     // socket.io ping teacher socket
   });
 
-  /* Handles instructor archive student post */
-  app.post('/student/posts/archive', function() {
+  /* Handles instructor archive student question */
+  app.post('/student/questions/archive', function() {
 
-  });
-
-  /* rendering start page */
-  app.get('/start', function(request, response) {
-    response.render('teacher/start.html');
   });
 }
