209910e3b9a23026137cf558eb25cb000713374a
Sherman Leung <shermankleung@gmail.com>
2014-11-19 21:36:20 -0800

    added dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var passport = require('passport');
 
 module.exports = function(app) {
-  /* Renders the newsfeed landing page. */
+  /* Renders the index and landing page. */
   app.get('/', function(request, response) {
     response.render('index.html');
   });
 
+  /* rendering login page */
+  app.get('/login', function(request, response) {
+    response.render('login.html');
+  });
+
+  /* rendering dashboard page */
+  app.get('/dashboard', function(request, response) {
+    response.render('dashboard.html');
+  });
+
+
+  // for passport authentication, signin with google
   app.get('/auth/google', passport.authenticate('google'));
 
   // Google will redirect the user to this URL after authentication.  Finish
