7bff56acb36767407495a86a4efa1a9415398227
Sherman Leung <shermankleung@gmail.com>
2014-12-05 17:34:02 -0800

    fixed iframe height

--- public/js/teacher/main.js
+++ public/js/teacher/main.js
@@ -2,5 +2,5 @@
 
   /* Render the MainView, which handles application logic. */
   MainView.render($(document.body));
-
+  $("#presentation").height($(window).height() - 140);
 })(this, this.document);
